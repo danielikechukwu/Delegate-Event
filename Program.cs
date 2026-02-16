b8d8b00b7140be66d92ca1064ff2ab66196552f9
Daniel Ikechukwu <Ikechukwudaniel080@gmail.com>
2026-02-17 00:06:13 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DelegateEvent
+{
+    public delegate void WorkPerformedHandler(int hours, );
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+        }
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegate_Event
+{
+
+    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+
+    public class Worker
+    {
+        public event WorkPerformedHandler workPerformed;
+
+        public event EventHandler WorkCompleted;
+
+        public void DoWork(int hours, WorkType workType)
+        {
+            //Raising event
+        }
+    }
+
+    public enum WorkType
+    {
+        Golf,
+        GotoMeetings,
+        GenerateReports
+    }
+}
